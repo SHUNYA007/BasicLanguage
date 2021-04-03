4e3b455fdf048eb71a289968e93375afc440318c
ADAPTIVE007 <32463461+ADAPTIVE007@users.noreply.github.com>
2021-04-04 01:05:27 +0530

    initial commit

---
+++ basic.py
@@ -0,0 +1,28 @@
+
+TokenInt='INT'
+TokenFloat='FLOAT'
+TokenString='STRING'
+TokenMul='MUL'
+TokenAdd='ADD'
+TokenSub='SUB'
+TokenLParen='LEFT PARANTHESIS'
+TokenRParen='RIGHT PARANTHESIS'
+class Token:
+    def __init__(self,argType,value):
+        self.argType=argType
+        self.value=value
+    def __repr__(self):
+        if self.value:
+            return f'{self.argType}:{self.value}'
+        return f'{self.argType}'
+
+
+class Lexer:
+    def __init__(self,text):
+        self.text=text
+        self.pos=-1
+        self.current_char=None
+        self.advance()
+    def advance(self):
+        self.pos+=1
+        self.current_char=self.text[pos] if self.pos<len(self.text) else None
